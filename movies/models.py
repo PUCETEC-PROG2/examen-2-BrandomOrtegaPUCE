b2f407e1596078dfb72b3348442c56020a5b6ebc
Brandom Ortega <baortega@puce.edu.ec>
2024-07-13 10:46:41 -0500

    examen 2do parcial

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Movies(models.Model):
+    title = models.CharField(max_length=30)
+    year = models.IntegerField()
+    director = models.CharField(max_length=50)
+    genre = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movies
 
 # Create your views here.
+def index(request):
+    movies = Movies.objects.order_by('type')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'movies': movies}, request))
+
+def movies(request, movies_id):
+    movie = Movies.objects.get(pk = movies_id)
+    template = loader.get_template('display_movies.html')
+    context = {
+        'movie': movie
+    }
+    return HttpResponse(template.render(context, request))
